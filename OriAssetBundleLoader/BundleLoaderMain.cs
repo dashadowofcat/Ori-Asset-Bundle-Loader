97aab582733abc58e1e1624bb6f9f351aa666169
dashadowofcat <104553632+dashadowofcat@users.noreply.github.com>
2024-04-23 14:41:13 +0300

    Add project files.

---
+++ OriAssetBundleLoader/BundleLoaderMain.cs
@@ -0,0 +1,88 @@
+﻿using Il2Cpp;
+using MelonLoader;
+using System.Linq;
+using UnityEngine;
+using UniverseLib;
+
+namespace OriAssetBundleLoader
+{
+    public class BundleLoaderMain : MelonMod
+    {
+        Il2CppAssetBundle bundle;
+
+        public override void OnApplicationStart()
+        {
+            bundle = Il2CppAssetBundleManager.LoadFromFile("Mods/assets/ori");
+        }
+
+
+        public override void OnUpdate()
+        {
+            if (UniverseLib.Input.InputManager.GetKeyDown(KeyCode.U))
+            {
+                LoadObject();
+            }
+        }
+
+        public void LoadObject()
+        {
+            GameObject Root = bundle.LoadAsset<GameObject>("prefab root");
+
+            GameObject obj = UnityEngine.Object.Instantiate(Root);
+
+            GameObject OriObj = UniverseLib.RuntimeHelper.FindObjectsOfTypeAll<GameObject>().Where(g => g.name == "seinCharacter").FirstOrDefault();
+
+            obj.transform.position = OriObj.transform.position;
+
+            Object.DontDestroyOnLoad(obj);
+
+            ConvertChildrenToWOTW(obj.transform);
+        }
+
+        void ConvertChildrenToWOTW(Transform parent)
+        {
+            foreach (Transform child in UniverseLib.RuntimeHelper.FindObjectsOfTypeAll<Transform>().Where(m => m.root == parent && m != parent))
+            {
+                ConvertAssetToWOTW(child.gameObject);
+            }
+        }
+
+
+        public void ConvertAssetToWOTW(GameObject Asset)
+        {
+            if (Asset.GetComponent<MeshRenderer>())
+            {
+                ConvertRendererToWOTW(Asset);
+                return;
+            }
+
+            if (Asset.GetComponent<Collider>())
+            {
+                ConvertColliderToWOTW(Asset);
+
+                return;
+            }
+        }
+
+        void ConvertRendererToWOTW(GameObject Asset)
+        {
+            MeshRenderer ObjRenderer = Asset.GetComponent<MeshRenderer>();
+
+            Texture2D ObjTexture = ObjRenderer.material.mainTexture.TryCast<Texture2D>();
+
+            ObjRenderer.material = Settings.OriMaterial;
+
+            ObjRenderer.material.color = Settings.EnviormentColor;
+
+            ObjRenderer.material.mainTexture = ObjTexture;
+
+            UberShaderAPI.SetTexture(ObjRenderer, UberShaderProperty_Texture.MultiplyLayerMaskTexture, null);
+            UberShaderAPI.SetTexture(ObjRenderer, UberShaderProperty_Texture.MultiplyLayerTexture, null);
+        }
+
+        void ConvertColliderToWOTW(GameObject Asset)
+        {
+            Asset.layer = 10;
+        } 
+    }
+}
---
+++ OriAssetBundleLoader/Settings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace OriAssetBundleLoader
+{
+    public static class Settings
+    {
+        public static Color EnviormentColor = new Color(1.8f, 1.6f, 1.4f);
+
+        public static Material OriMaterial
+        {
+            get { return GameObject.Find("seinCharacter/ori3D/mirrorHolder/rigHolder/oriRig/Model_GRP/body_MDL").GetComponent<SkinnedMeshRenderer>().material; }
+        }
+    }
+}
