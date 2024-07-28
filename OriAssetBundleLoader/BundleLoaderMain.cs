f3fd2715c7d28c7b635c976b5d00a003581ca5b8
dashadowofcat <104553632+dashadowofcat@users.noreply.github.com>
2024-07-28 14:19:56 +0300

    fixed input issue

--- OriAssetBundleLoader/BundleLoaderMain.cs
+++ OriAssetBundleLoader/BundleLoaderMain.cs
@@ -3,16 +3,15 @@ using Il2CppMoon;
 using MelonLoader;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UniverseLib;
 using HarmonyLib;
 using System;
 using System.Collections.Generic;
 using Il2CppSystem.Collections.Generic;
-using Il2CppInterop.Runtime.Injection;
-using System.Collections;
 using Il2CppSystem.IO;
 using UniverseLib.Input;
+using UniverseLib.Config;
+using MelonLoader.Utils;
 
 namespace OriAssetBundleLoader
 {
@@ -24,10 +23,20 @@ namespace OriAssetBundleLoader
 
         PrefabManager PrefabManager = new PrefabManager();
 
-        public override void OnApplicationStart()
+        public override void OnInitializeMelon()
         {
-            if (!File.Exists("Mods/UnityExplorer.ML.IL2CPP.net6preview.interop.dll"))
+
+            if (!File.Exists($"{MelonEnvironment.ModsDirectory}/UnityExplorer.ML.IL2CPP.net6preview.interop.dll"))
             {
+                //string UnhollowedModulesFolder = Path.Combine(Path.GetDirectoryName(MelonEnvironment.ModsDirectory), Path.Combine("MelonLoader", "Managed"));
+
+                //Universe.Init(0, OnUniverseInit, OnUniverseLog, new UniverseLibConfig()
+                //{
+                //    Disable_EventSystem_Override = false,
+                //    Force_Unlock_Mouse = false,
+                //    Unhollowed_Modules_Folder = UnhollowedModulesFolder
+                //});
+
                 Universe.Init();
             }
 
@@ -56,17 +65,17 @@ namespace OriAssetBundleLoader
         public override void OnUpdate()
         {
 
-            if (InputManager.GetMouseButton(2))
+            if (MoonInput.GetMouseButton(2))
             {
                 MelonLogger.Msg("click");
             }
 
-            if (InputManager.GetKeyDown(KeyCode.U))
+            if (MoonInput.GetKeyDown(KeyCode.U))
             {
                 LoadObject();
             }
 
-            if (InputManager.GetKeyDown(KeyCode.Y))
+            if (MoonInput.GetKeyDown(KeyCode.Y))
             {
                 GameObject.Find("systems/scenesManager").GetComponent<GoToSceneController>().GoToScene("stressTestMaster");
             }
