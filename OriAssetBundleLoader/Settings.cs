b6cf96cbf20bea6e8bb11765503530b22ed9f9f7
dashadowofcat <104553632+dashadowofcat@users.noreply.github.com>
2024-07-28 20:48:18 +0300

    minor changes

--- OriAssetBundleLoader/Settings.cs
+++ OriAssetBundleLoader/Settings.cs
@@ -9,7 +9,7 @@ namespace OriAssetBundleLoader
 {
     public static class Settings
     {
-        public static Color EnvironmentColor = new Color(1, 1, 1);
+        public static Color EnvironmentColor = new Color(1.8f, 1.6f, 1.4f);
 
         public static Material EnvironmentMaterial => GameObject.Find("seinCharacter/ori3D/mirrorHolder/rigHolder/oriRig/Model_GRP/body_MDL").GetComponent<SkinnedMeshRenderer>().material;
 
