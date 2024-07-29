a512a2b308f30f5f0a5836a17db10b487d5d3ae4
BenjaminNelson <s-benjamin.nelson@lwtech.edu>
2024-07-29 12:58:20 -0400

    Began Implementation of Life Plant converter and prefab asset

--- OriAssetBundleLoader/PrefabManager.cs
+++ OriAssetBundleLoader/PrefabManager.cs
@@ -12,9 +12,12 @@ using UniverseLib;
 public class PrefabManager
 {
     public static GameObject spring;
+    public static GameObject lifePlant;
 
     public System.Collections.IEnumerator SetupPrefabs()
     {
+        // Cache Spring Plant
+
         SceneManager.LoadScene("springIntroCavernA", LoadSceneMode.Additive);
 
         while (RuntimeHelper.FindObjectsOfTypeAll<Spring>().Length < 1) yield return new WaitForFixedUpdate();
@@ -22,5 +25,16 @@ public class PrefabManager
         spring = RuntimeHelper.FindObjectsOfTypeAll<Spring>().FirstOrDefault().transform.parent.gameObject;
 
         MelonLogger.Msg("Spring Loaded");
+
+        // Cache Life Plant
+
+        SceneManager.LoadScene("kwoloksCavernO", LoadSceneMode.Additive);
+
+        while (RuntimeHelper.FindObjectsOfTypeAll<OrbSpawner>().Where(g => g.name == "landOnAndSpawnOrbs").Count() < 1) yield return new WaitForFixedUpdate();
+
+        lifePlant = RuntimeHelper.FindObjectsOfTypeAll<OrbSpawner>().Where(g => g.name == "landOnAndSpawnOrbs").FirstOrDefault().gameObject;
+
+        MelonLogger.Msg("Life Plant Loaded");
+
     }
 }
