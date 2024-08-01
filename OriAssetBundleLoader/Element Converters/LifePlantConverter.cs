6fb747485bfaa59a4bad183071ca036f4a42d3b3
dashadowofcat <104553632+dashadowofcat@users.noreply.github.com>
2024-08-01 20:55:00 +0300

    disabled extra art that isnt necessary

--- OriAssetBundleLoader/Element Converters/LifePlantConverter.cs
+++ OriAssetBundleLoader/Element Converters/LifePlantConverter.cs
@@ -19,6 +19,8 @@ public class LifePlantConverter : ElementConverter
 
         OrbSpawner orbSpawner = lifePlant.GetComponent<OrbSpawner>();
 
+        lifePlant.transform.Find("deathEffect").gameObject.SetActive(false);
+
         orbSpawner.IdealOrbCount = this.GetInt(Asset, "IdealOrbs");
 
         orbSpawner.NumberOfHealthOrbs = this.GetInt(Asset, "NumberOfHealthOrbs");
