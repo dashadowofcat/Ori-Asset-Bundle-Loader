c68529f6da8bba6eb79515fa9519c08372a38b59
dashadowofcat <104553632+dashadowofcat@users.noreply.github.com>
2024-08-15 23:22:23 +0300

    fixed rotating hazard caching

--- OriAssetBundleLoader/Element Converters/RotatingSpikeHazardConverter.cs
+++ OriAssetBundleLoader/Element Converters/RotatingSpikeHazardConverter.cs
@@ -10,13 +10,11 @@ public class RotatingSpikeHazardConverter : ElementConverter
 {
     public override void ConvertElement(GameObject Asset)
     {
-        GameObject rotatingHazard = GameObject.Instantiate(PrefabManager.spring, Asset.transform);
+        GameObject rotatingHazard = GameObject.Instantiate(PrefabManager.rotatingSpikeHazaard, Asset.transform);
 
         rotatingHazard.transform.localPosition = Vector3.zero;
 
-        rotatingHazard.GetComponentInChildren<AutoRotate>().Speed = this.GetFloat(Asset, "Speed");
-
-        //rotatingHazard.GetComponentInChildren<AutoRotate>().m_rotation = this.GetFloat(Asset, "Rotation");
+        //rotatingHazard.GetComponentInChildren<AutoRotate>().Speed = this.GetFloat(Asset, "Speed");
     }
 }
 
