6c2eb9f74f2f5d0ea0064d4f594ec13af3c0b7a8
BenjaminNelson <s-benjamin.nelson@lwtech.edu>
2024-08-22 12:55:12 -0400

    Began implementation of keystone and 2 slot door converter. I ran into problems. Check the code comments

---
+++ OriAssetBundleLoader/Element Converters/KeystoneConverter.cs
@@ -0,0 +1,20 @@
+﻿using Il2Cpp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace OriAssetBundleLoader.Element_Converters
+{
+    public class KeystoneConverter : ElementConverter
+    {
+        public override void ConvertElement(GameObject Asset)
+        {
+            GameObject keystone = GameObject.Instantiate(PrefabCachingManager.GetPrefab("Keystone"), Asset.transform);
+
+            keystone.transform.localPosition = Vector3.zero;
+        }
+    }
+}
---
+++ OriAssetBundleLoader/Element Converters/TwoSlotDoorConverter.cs
@@ -0,0 +1,20 @@
+﻿using Il2Cpp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace OriAssetBundleLoader.Element_Converters
+{
+    public class TwoSlotDoorConverter : ElementConverter
+    {
+        public override void ConvertElement(GameObject Asset)
+        {
+            GameObject twoSlotDoor = GameObject.Instantiate(PrefabCachingManager.GetPrefab("TwoSlotDoor"), Asset.transform);
+
+            twoSlotDoor.transform.localPosition = Vector3.zero;
+        }
+    }
+}
